74dda15c54fec49e746dfc881ac49a693877e7af
unknown <jolaine6868@gmail.com>
2025-12-12 12:17:53 +0100

    feat: create funtion RemovePhrases

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RemovePhrases from './RemovePhrases'
+
+
 
 function App() {
 return (
-    <>
-       
+  <>
+    <RemovePhrases></RemovePhrases>
     </>
   )
 }
 
 export default App
+
+
