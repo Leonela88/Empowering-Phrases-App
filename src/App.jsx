0ec4694aa1339b51e09f8cb47bd284caa295e9d8
Guadalupe <penaegeaguadalupe@gmail.com>
2025-12-14 21:32:26 +0100

    feat:create and implement fully responsive Header component based on design specifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import ViewPhrases from './ViewPhrases'
 import RemovePhrases from './RemovePhrases'
+import Header from './Header'
 
 function App() {
 return (
     <>
+    <Header></Header>
     <ViewPhrases></ViewPhrases>
     <RemovePhrases></RemovePhrases>
     </>
--- src/RemovePhrases.jsx
+++ src/RemovePhrases.jsx
@@ -5,7 +5,11 @@ const RemovePhrases = () => {
     "It's time to unleash your potential with our new service. Start your journey today! You will crush your goals and elevate your business. Level up your life now.";
 
   return (
-    div 
+    <>
+    </>
+    // <div>
+    //     <p>{websiteCopy}</p>
+    // </div>
   );
 };
 
