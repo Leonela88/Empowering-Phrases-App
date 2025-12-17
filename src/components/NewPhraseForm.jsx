1cc8fec474ce96dd6d87fd933653fb2d9292749d
Leonela88 <rivasaela@gmail.com>
2025-12-17 16:21:13 +0100

    fixed styles $ browse image

--- src/components/NewPhraseForm.jsx
+++ src/components/NewPhraseForm.jsx
@@ -5,50 +5,68 @@ const NewPhraseForm = () => {
 
   const [phrase, setPhrase] = useState('');
   const [author, setAuthor] = useState('');
+  const [image, setImage] = useState(null);
+
+<<<<<<< Updated upstream
+  const handleSubmit = (event) => {
+    event.preventDefault();
+=======
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.size < 2 * 1024 * 1024) { 
+      setImage(file);
+    } else {
+      alert("Archivo demasiado grande");
+    }
+  };
+>>>>>>> Stashed changes
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log('Nueva Frase Creada:', { phrase, author });
+    console.log('Nueva Frase Creada:', { phrase, author, image });
     setPhrase('');
     setAuthor('');
   };
 
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
 
       <div className={styles.img}>
-       
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          id="upload-photo"
+        />
       </div>
 
       <form onSubmit={handleSubmit} >
 
-       
+
         <div className={styles.phrasePanel}>
           <span className={styles.quoteMark}>&ldquo;</span>
 
           <textarea
             id="phrase"
             value={phrase}
-            onChange={(e) => setPhrase(e.target.value)}
+            onChange={(event) => setPhrase(event.target.value)}
             rows="4"
             placeholder="Please enter your phrase."
           ></textarea>
 
-     
+
           <div className={styles.authorContainer}>
             <div className={styles.authorLine}></div>
             <input
               type="text"
               id="author"
               value={author}
-              onChange={(e) => setAuthor(e.target.value)}
+              onChange={(event) => setAuthor(event.target.value)}
               placeholder="Enter the author."
             />
           </div>
         </div>
-
-        
         <div className={styles.footer}>
           <p>
             Thank you for your contribution and for continuing to grow the
@@ -56,15 +74,17 @@ const NewPhraseForm = () => {
             Please review that all fields have been filled in correctly and
             press the verify button.
           </p>
-
-         
-          <button type="submit" className={styles.verifyButton}>
-            <span className={styles.checkIcon}>&#10003;</span> 
+          <button type='submit'>
+            <img
+              src="/images/ok.png"
+              alt="Icono de verificación"
+            />
           </button>
         </div>
       </form>
-    </div>
+    </main>
   );
 };
+
 
 export default NewPhraseForm;
