2708f6cd54a5defcbf2d1641b0efb45b4ac67ca1
Guadalupe <penaegeaguadalupe@gmail.com>
2025-12-18 13:36:47 +0100

    feat: setup react-router-dom and define main application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import './App.css'
 import Header from './Header'
 import Footer from './Footer'
-import ViewPhrases from './ViewPhrases'
-import NewPhraseForm from './components/NewPhraseForm'
-import CreateReadActions from './CreateReadActions'
-import RemovePhrases from './RemovePhrases'
-import ViewAllPhrases from './ViewAllPhrases'
+import { Outlet } from 'react-router'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        {<ViewPhrases></ViewPhrases>}
-        {/* {< NewPhraseForm></NewPhraseForm>} */}
-        {/* <CreateReadActions></CreateReadActions> */}
-        {/* <RemovePhrases></RemovePhrases> */}
-        {/* <ViewAllPhrases/> */}
+      <Outlet/>
       <Footer/>
     </div>
   );
