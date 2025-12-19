ac166d9b55707c78555c3610d01b6846f6fa9f79
ManonChab <godfroymanon@gmail.com>
2025-12-19 10:33:23 +0100

    need to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import ViewPhrases from './components/View-Phrases/ViewPhrases'
-import NewPhraseForm from './components/New-Phrases-Form/NewPhraseForm'
-import CreateReadActions from './components/Create-Read-Actions/CreateReadActions'
-import RemovePhrases from './components/Remove-Phrases/RemovePhrases'
-import ViewAllPhrases from './components/View-All-Phrases/ViewAllPhrases'
+import { Outlet } from 'react-router'
 
 function App() {
   return (
-    <>
+    <div>
       <Header/>
       <Outlet/>
       <Footer/>
-    </>
+    </div>
   );
 }
 
